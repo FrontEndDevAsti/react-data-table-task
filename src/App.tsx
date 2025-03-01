da99c6a70f4f542d489c66a92507b3a991a5b1ff
FrontEndDevAsti <developer1@astiacademy.ac.ae>
2025-03-01 09:30:38 +0400

    homepage_redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import Users from './pages/Users';
-import Products from './pages/Products';
-import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Users from "./pages/Users";
+import Products from "./pages/Products";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -13,6 +18,7 @@ function App() {
           <Navbar />
           <main className="container mx-auto px-4 py-8">
             <Routes>
+              <Route path="/" element={<Navigate to="/users" replace />} />
               <Route path="/users" element={<Users />} />
               <Route path="/products" element={<Products />} />
             </Routes>
